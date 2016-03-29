55582bba8df1dbf20c11350cf11d137863ad84e8
Aaron Bentley <abentley@cylancecorp.com>
2016-03-29 11:28:32 -0700

    Adding initial test

---
+++ testApp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace testApp
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void dummyTest()
+        {
+            Console.WriteLine("This is a dummy test");
+        }
+    }
+}
